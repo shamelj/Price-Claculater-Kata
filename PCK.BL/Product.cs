94f5d256304963e4cccd6887b4a4d0a43f70c5d7
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-01 15:00:33 +0300

    TAX | Created Product class.

---
+++ PCK.BL/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace PCK.BL
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public uint UPC { get; set; }
+        public double BasePrice { get; set; }
+        public Product(string name, uint UPC, double basePrice)
+        {
+            this.Name = name;
+            this.UPC = UPC;
+            this.BasePrice = basePrice;
+        }
+    }  
+}
