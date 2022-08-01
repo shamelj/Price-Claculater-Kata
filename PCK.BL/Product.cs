4abdae7fee9d6768fd5a175fa897533cac36fa6e
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-01 17:26:34 +0300

    TAX | Added price object to product class

--- PCK.BL/Product.cs
+++ PCK.BL/Product.cs
@@ -1,15 +1,17 @@
-﻿namespace PCK.BL
+﻿using PCK.Utility;
+
+namespace PCK.BL
 {
     public class Product
     {
         public string Name { get; set; }
         public uint UPC { get; set; }
-        public double BasePrice { get; set; }
-        public Product(string name, uint UPC, double basePrice)
+        public Price BasePrice { get; set; }
+        public Product(string name, uint UPC, Price basePrice)
         {
             this.Name = name;
             this.UPC = UPC;
             this.BasePrice = basePrice;
         }
-    }  
+    }
 }
