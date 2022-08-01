5e08c47b74869e01941c87f4ceffd71d57aa3781
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-02 02:26:50 +0300

    REPORT | Added Report class

---
+++ PCK.BL/Report.cs
@@ -0,0 +1,17 @@
+﻿using PCK.Utility;
+
+namespace PCK.BL
+{
+    public class Report
+    {
+        public static void ReprotDicount(Price amount)
+        {
+            if (amount.Value != 0.00)
+                Console.WriteLine($"Discount {amount.Value}");
+        }
+        public static void ReportNetPrice(Price amount)
+        {
+            Console.WriteLine($"Price {amount.Value}");
+        }
+    }
+}
