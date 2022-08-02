05ba08d13e8c8179fd74f1655d649eea5d6a7947
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-02 18:48:23 +0300

    SELECTIVE | Added basic interfaces to do some refactoring on Calculater class.

---
+++ PCK.BL/Intefaces/IDiscountsRepository.cs
@@ -0,0 +1,11 @@
+﻿using PCK.BL.Entities;
+
+namespace PCK.BL.Intefaces
+{
+    public interface IDiscountsRepository
+    {
+        IEnumerable<Discount> ReadAll();
+        IEnumerable<Discount> ReadAll(uint upc);
+        void Save(Discount discount);
+    }
+}
---
+++ PCK.BL/Intefaces/IReporter.cs
@@ -0,0 +1,10 @@
+﻿using PCK.Utility;
+
+namespace PCK.BL.Intefaces
+{
+    public interface IReporter
+    {
+        void Discounted(Price amount);
+        void NetPrice(Price amount);
+    }
+}
