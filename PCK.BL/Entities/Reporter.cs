d0571b018e1e61995111a2d2673d1374d52a4fc2
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-02 19:01:58 +0300

    SELECTIVE | Added IReporter interface.

---
+++ PCK.BL/Entities/Reporter.cs
@@ -0,0 +1,18 @@
+﻿using PCK.BL.Intefaces;
+using PCK.Utility;
+
+namespace PCK.BL.Entities
+{
+    public class Reporter : IReporter
+    {
+        public void Discounted(Price amount)
+        {
+            if (amount.Value != 0.00)
+                Console.WriteLine($"Discount {amount.Value}");
+        }
+        public void NetPrice(Price amount)
+        {
+            Console.WriteLine($"Price {amount.Value}");
+        }
+    }
+}
