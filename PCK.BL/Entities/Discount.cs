32f751dedaf205d78552278cc9c49536f6a2fc63
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-02 18:58:58 +0300

    SELECTIVE | Added Discount and DiscountCalculater classes

---
+++ PCK.BL/Entities/Discount.cs
@@ -0,0 +1,24 @@
+﻿namespace PCK.BL.Entities
+{
+    public class Discount
+    {
+        private double _rate;
+
+        public double Rate
+        {
+            get => _rate;
+            set
+            {
+                if (value < 0 || value > 1)
+                    throw new ArgumentOutOfRangeException("Rate must be in range [0,1]");
+                _rate = value;
+            }
+        }
+        public uint UPC { get; set; }
+        public Discount(double rate, uint upc)
+        {
+            Rate = rate;
+            UPC = upc;
+        }
+    }
+}
