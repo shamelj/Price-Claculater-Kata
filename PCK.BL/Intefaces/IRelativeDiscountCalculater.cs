63eeefa1696981d2331d19e504ec570dc186eb37
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-03 13:04:54 +0300

    PRECEDENCE | Made interface segeregation to to discount class, interface for each type of discounts.

---
+++ PCK.BL/Intefaces/IRelativeDiscountCalculater.cs
@@ -0,0 +1,10 @@
+﻿using PCK.BL.Entities;
+using PCK.Utility;
+
+namespace PCK.BL.Intefaces
+{
+    public interface IRelativeDiscountCalculater
+    {
+        Price CalculateRelativeDiscount(Product product);
+    }
+}
