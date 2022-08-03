a347401548c16e2cc9ec0e0162f9318f0b6eefe4
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-03 13:13:25 +0300

    PRECEDENCE | Added the Precedence requirement and modified tax calculator.

---
+++ PCK.BL/Entities/NetPriceCalculator.cs
@@ -0,0 +1,35 @@
+﻿using PCK.BL.Intefaces;
+using PCK.Utility;
+
+namespace PCK.BL.Entities
+{
+    public class NetPriceCalculator
+    {
+        private IDiscountCalculator DiscountCalculator { get; init; }
+        private IFlatTaxCalculator TaxCalculator { get; init; }
+        private IReporter Reporter { get; init; }
+        public NetPriceCalculator(IDiscountCalculator discountCalculater, IFlatTaxCalculator taxCalculator, IReporter reporter)
+        {
+            DiscountCalculator = discountCalculater;
+            TaxCalculator = taxCalculator;
+            Reporter = reporter;
+        }
+
+        public Price CalculateNetPrice(Product product)
+        {
+            var preceedingDiscount = DiscountCalculator.CalculatePreceedingDiscount(product);
+            var basePriceWithPreceedingDiscount = product.BasePrice - preceedingDiscount;
+            var productWithNewPrice = new Product(product) { BasePrice = basePriceWithPreceedingDiscount };
+            var nonPreceedingDiscount = DiscountCalculator.CalculateNonPreceedingDiscount(productWithNewPrice);
+            var relativeDiscount = DiscountCalculator.CalculateRelativeDiscount(productWithNewPrice);
+            var totalDiscount = preceedingDiscount + nonPreceedingDiscount + relativeDiscount;
+            var flatTax = TaxCalculator.CalculateFlatTax(productWithNewPrice);
+            var netPrice = product.BasePrice - totalDiscount + flatTax;
+            Reporter.NetPrice(netPrice);
+            Reporter.Discounted(totalDiscount);
+            return netPrice;
+        }
+
+
+    }
+}
