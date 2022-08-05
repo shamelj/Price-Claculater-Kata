17b4d42195cf7031ca78271fdb6091cf79b77dab
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-05 17:28:09 +0300

    COMBINING | Refactored DiscountCalculator and made it an abstract class.

--- PCK.BL/Entities/DiscountCalculator.cs
+++ PCK.BL/Entities/DiscountCalculator.cs
@@ -3,35 +3,16 @@ using PCK.Utility;
 
 namespace PCK.BL.Entities
 {
-    public class DiscountCalculator : IDiscountCalculator
+    public abstract class DiscountCalculator : IDiscountCalculator
     {
         public static double RelativeDiscountRate { get; set; } = 0;
-        private IDiscountsRepository DiscountsRepository { get; }
+        protected IDiscountsRepository DiscountsRepository { get; init; }
         public DiscountCalculator(IDiscountsRepository discountsRepository)
         {
             DiscountsRepository = discountsRepository;
         }
-        
-        public Price CalculateNonPreceedingDiscount(Product product)
-        {
-            var discounts = DiscountsRepository.ReadAll(product.UPC);
-            var nonPreceedingDiscounts = discounts.Where((discount) => discount.Type == DiscountType.NonPreceeding);
-            var totalDiscountsRate = nonPreceedingDiscounts.Sum((discount) => discount.Rate);
-            var totalNonPreceedingDiscountAmount = new Price(totalDiscountsRate * product.BasePrice.Value);
-            return totalNonPreceedingDiscountAmount;
-        }
-        public Price CalculatePreceedingDiscount(Product product)
-        {
-            var allDiscounts = DiscountsRepository.ReadAll(product.UPC);
-            var preceedingDiscounts = allDiscounts.Where((discount) => discount.Type == DiscountType.Preceeding);
-            var totalDiscountsRate = preceedingDiscounts.Sum((discount) => discount.Rate);
-            var totalPreceedingDiscountAmount = new Price(totalDiscountsRate * product.BasePrice.Value);
-            return totalPreceedingDiscountAmount;
-        }
-        public Price CalculateRelativeDiscount(Product product)
-        {
-            var relativeDiscount = new Price(product.BasePrice.Value * RelativeDiscountRate);
-            return relativeDiscount;
-        }
+        public abstract Price CalculateNonPreceedingDiscount(Product product);
+        public abstract Price CalculatePreceedingDiscount(Product product);
+        public abstract Price CalculateRelativeDiscount(Product product);
     }
 }
