6d255cab664817a8636117bea3bfe889159ef1b3
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-06 02:12:57 +0300

    CAP | Added cap calculation classes.

---
+++ PCK.BL/Entities/RelativeCapCalculator.cs
@@ -0,0 +1,22 @@
+﻿using PCK.BL.Intefaces;
+using PCK.Utility;
+
+namespace PCK.BL.Entities
+{
+    public class RelativeCapCalculator : ICapCalculator
+    {
+        private readonly double CAPRate;
+
+        public RelativeCapCalculator(double capRate)
+        {
+            if (capRate < 0)
+                throw new ArgumentException("CAP rate must be positive.");
+            CAPRate = capRate;
+        }
+
+        public Price CalculateCAP(Product product)
+        {
+            return new(product.BasePrice.Value * CAPRate);
+        }
+    }
+}
