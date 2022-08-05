a9b4ca2b531a353b77425998c9043af1afed6f4a
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-06 02:14:11 +0300

    CAP | added cap functionality into net price calculator, and added the test case.

--- PCK.BL/Entities/NetPriceCalculator.cs
+++ PCK.BL/Entities/NetPriceCalculator.cs
@@ -8,34 +8,41 @@ namespace PCK.BL.Entities
         private IDiscountCalculator DiscountCalculator { get; init; }
         private IFlatTaxCalculator TaxCalculator { get; init; }
         private IReporter Reporter { get; init; }
-        public IExpensesCalculator ExpensesCalculator { get; private set; }
+        private IExpensesCalculator ExpensesCalculator { get; init; }
+        private ICapCalculator CapCalculator { get; init; }
+
 
         public NetPriceCalculator(IDiscountCalculator discountCalculater,
                                   IFlatTaxCalculator taxCalculator,
                                   IReporter reporter,
-                                  IExpensesCalculator expensesCalculator)
+                                  IExpensesCalculator expensesCalculator,
+                                  ICapCalculator capCalculator)
         {
             DiscountCalculator = discountCalculater;
             TaxCalculator = taxCalculator;
             Reporter = reporter;
             ExpensesCalculator = expensesCalculator;
+            CapCalculator = capCalculator;
         }
 
         public Price CalculateNetPrice(Product product)
         {
+            var cap = CapCalculator.CalculateCAP(product);
             var preceedingDiscount = DiscountCalculator.CalculatePreceedingDiscount(product);
+            preceedingDiscount = preceedingDiscount > cap ? cap : preceedingDiscount;
             var basePriceWithPreceedingDiscount = product.BasePrice - preceedingDiscount;
             var productWithNewPrice = new Product(product) { BasePrice = basePriceWithPreceedingDiscount };
             var nonPreceedingDiscount = DiscountCalculator.CalculateNonPreceedingDiscount(productWithNewPrice);
             var relativeDiscount = DiscountCalculator.CalculateRelativeDiscount(productWithNewPrice);
             var totalDiscount = preceedingDiscount + nonPreceedingDiscount + relativeDiscount;
+            totalDiscount = totalDiscount > cap ? cap : totalDiscount;
             var flatTax = TaxCalculator.CalculateFlatTax(productWithNewPrice);
             var absoluteExpenses = ExpensesCalculator.CalculateAbsoluteExpenses();
             var relativeExpenses = ExpensesCalculator.CalculateRelativeExpenses(productWithNewPrice);
             var totalExpenses = absoluteExpenses + relativeExpenses;
             var netPrice = product.BasePrice - totalDiscount + flatTax + totalExpenses;
-            var allExpenses = ExpensesCalculator.AllExpenses(productWithNewPrice);
-            Reporter.Summary(product.BasePrice, flatTax, totalDiscount, allExpenses, netPrice);
+            var allExpensesData = ExpensesCalculator.AllExpensesData(productWithNewPrice);
+            Reporter.Summary(product.BasePrice, flatTax, totalDiscount, allExpensesData, netPrice);
             Reporter.Discounted(totalDiscount);
             return netPrice;
         }
