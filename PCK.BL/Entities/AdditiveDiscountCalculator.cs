88381f9a4e2f0cf5fcd56da26873561141fd181b
shamelj <70355802+shamelj@users.noreply.github.com>
2022-08-05 17:29:31 +0300

    COMBINING | Made Additive and Multiplicative calculaters.

---
+++ PCK.BL/Entities/AdditiveDiscountCalculator.cs
@@ -0,0 +1,34 @@
+﻿using PCK.BL.Intefaces;
+using PCK.Utility;
+
+namespace PCK.BL.Entities
+{
+    public class AdditiveDiscountCalculator : DiscountCalculator
+    {
+        public AdditiveDiscountCalculator(IDiscountsRepository discountsRepository) : base(discountsRepository)
+        {
+        }
+
+        public override Price CalculateNonPreceedingDiscount(Product product)
+        {
+            var discounts = DiscountsRepository.ReadAll(product.UPC);
+            var nonPreceedingDiscounts = discounts.Where((discount) => discount.Type == DiscountType.NonPreceeding);
+            var totalDiscountsRate = nonPreceedingDiscounts.Sum((discount) => discount.Rate);
+            var totalNonPreceedingDiscountAmount = new Price(totalDiscountsRate * product.BasePrice.Value);
+            return totalNonPreceedingDiscountAmount;
+        }
+        public override Price CalculatePreceedingDiscount(Product product)
+        {
+            var allDiscounts = DiscountsRepository.ReadAll(product.UPC);
+            var preceedingDiscounts = allDiscounts.Where((discount) => discount.Type == DiscountType.Preceeding);
+            var totalDiscountsRate = preceedingDiscounts.Sum((discount) => discount.Rate);
+            var totalPreceedingDiscountAmount = new Price(totalDiscountsRate * product.BasePrice.Value);
+            return totalPreceedingDiscountAmount;
+        }
+        public override Price CalculateRelativeDiscount(Product product)
+        {
+            var relativeDiscount = new Price(product.BasePrice.Value * RelativeDiscountRate);
+            return relativeDiscount;
+        }
+    }
+}
